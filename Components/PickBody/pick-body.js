558031b6a0f017323052155c335e03e3382a041f
Jagiela <kjagiela93@gmail.com>
2024-06-05 20:35:17 +0200

    Experiencing with new Pick Column component, fixed mobile view, added inner shadow to buttons

--- Components/PickBody/pick-body.js
+++ Components/PickBody/pick-body.js
@@ -3,6 +3,7 @@ import classes from "./pick-body.module.css";
 import Image from "next/image";
 import { useState } from "react";
 
+import PlayerPickColumn from "../PlayerPickColumn/player-pick-column";
 import paperIcon from "@/public/icon-paper.svg";
 import scissorsIcon from "@/public/icon-scissors.svg";
 import rockIcon from "@/public/icon-rock.svg";
@@ -11,39 +12,24 @@ export default function PickBody({playerPick, housePick, winner, restartGame}) {
 
     return(
         <div className={classes.pickBodyWraper}>
-            <div className={classes.pickColumn}>
-                <p>YOU PICKED</p>
-                <button className={playerPick === "paper" ? classes.paperButton : playerPick === "scissors" ? classes.scissorsButton : playerPick === "rock" ? classes.rockButton : null} >
-                    <Image 
-                    src={playerPick === "paper" ? paperIcon : playerPick === "scissors" ? scissorsIcon : playerPick === "rock" ? rockIcon : null} 
-                    alt={playerPick + " Icon"} 
-                    className={classes.buttonIcon}/>
-                </button>
-                <div className={winner==="Player" ? classes.circleWave : classes.noWave}></div>
-                <div className={winner==="Player" ? classes.circleWave2 : classes.noWave}></div>
-                <div className={winner==="Player" ? classes.circleWave3 : classes.noWave}></div>
-            </div>
+            <PlayerPickColumn 
+                text="YOU PICKED"
+                playerPick={playerPick}
+                playerName="Player"
+                winner={winner}
+            />
             {winner ? 
                 <div className={classes.pickResultColumn}>
                     <h1>{winner==="Player" ? "YOU WIN" : winner==="House" ? "YOU LOSE" : "DRAW"}</h1>
                     <button onClick={() => {restartGame()}}>PLAY AGAIN</button>
                 </div> 
                 : null}
-
-            <div className={classes.pickColumn}>
-                <p>THE HOUSE PICKED</p>
-                {housePick ? 
-                    <button className={housePick === "paper" ? classes.paperButton : housePick === "scissors" ? classes.scissorsButton : housePick === "rock" ? classes.rockButton : null} >
-                        <Image 
-                            src={housePick === "paper" ? paperIcon : housePick === "scissors" ? scissorsIcon : housePick === "rock" ? rockIcon : null} 
-                            alt={housePick + " Icon"} 
-                            className={classes.buttonIcon}/>
-                    </button>
-                : <div className={classes.pickLoaderContainer}><div className={classes.pickLoader}></div></div>}
-                <div className={winner==="House" ? classes.circleWave : classes.noWave}></div>
-                <div className={winner==="House" ? classes.circleWave2 : classes.noWave}></div>
-                <div className={winner==="House" ? classes.circleWave3 : classes.noWave}></div>
-            </div>
+            <PlayerPickColumn 
+                text="THE HOUSE PICKED"
+                playerPick={housePick}
+                playerName="House"
+                winner={winner}
+            />
         </div>
     );
 }
--- Components/PlayerPickColumn/player-pick-column.js
+++ Components/PlayerPickColumn/player-pick-column.js
@@ -9,15 +9,17 @@ export default function PlayerPickColumn({text, playerPick, playerName, winner})
     return(
         <div className={classes.pickColumn}>
             <p>{text}</p>
-            <button className={playerPick === "paper" ? classes.paperButton : playerPick === "scissors" ? classes.scissorsButton : playerPick === "rock" ? classes.rockButton : null} >
-                <Image 
-                src={playerPick === "paper" ? paperIcon : playerPick === "scissors" ? scissorsIcon : playerPick === "rock" ? rockIcon : null} 
-                alt={playerPick + " Icon"} 
-                className={classes.buttonIcon}/>
-            </button>
-            <div className={winner=={playerName} ? classes.circleWave : classes.noWave}></div>
-            <div className={winner=={playerName} ? classes.circleWave2 : classes.noWave}></div>
-            <div className={winner=={playerName} ? classes.circleWave3 : classes.noWave}></div>
+            {playerPick ? 
+                <button className={playerPick === "paper" ? classes.paperButton : playerPick === "scissors" ? classes.scissorsButton : playerPick === "rock" ? classes.rockButton : null} >
+                    <Image 
+                        src={playerPick === "paper" ? paperIcon : playerPick === "scissors" ? scissorsIcon : playerPick === "rock" ? rockIcon : null} 
+                        alt={playerPick + " Icon"} 
+                        className={classes.buttonIcon}/>
+                </button>
+            : <div className={classes.pickLoaderContainer}><div className={classes.pickLoader}></div></div>}
+            <div className={winner==playerName ? classes.circleWave : classes.noWave}></div>
+            <div className={winner==playerName ? classes.circleWave2 : classes.noWave}></div>
+            <div className={winner==playerName ? classes.circleWave3 : classes.noWave}></div>
         </div>
     );
 }
